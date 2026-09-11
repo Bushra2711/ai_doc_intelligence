e372d3ce914ea89aa5e314cc0522f0995a96abe2
Bushra Abdul Faheem <bushra14@amityonline.com>
2026-09-11 16:32:50 +0530

    style: load confidence score styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import "./styles/globals.css";
+import "./styles/confidence.css";
 
 ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
